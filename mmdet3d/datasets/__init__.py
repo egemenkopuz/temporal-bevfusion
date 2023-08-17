df43f29f8c0b62fe8d266fd37e0524dcfbf16346
Egemen <egemenkopuz@hotmail.com>
2023-08-17 17:07:26 +0200

    feat: add osdar23 dataset implementation

--- mmdet3d/datasets/__init__.py
+++ mmdet3d/datasets/__init__.py
@@ -3,6 +3,7 @@ from mmdet.datasets.builder import build_dataloader
 from .builder import *
 from .custom_3d import *
 from .nuscenes_dataset import *
+from .osdar23_dataset import *
 from .pipelines import *
 from .tumtraf_intersection_dataset import *
 from .utils import *
