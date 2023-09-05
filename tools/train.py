043c1437d53cf8eb0067ca6a1e7c71b7e2520710
Egemen Kopuz <egemen.kopuz@tum.de>
2023-09-05 14:01:37 +0000

    feat: add auto run directory finding and creation to train script

--- tools/train.py
+++ tools/train.py
@@ -1,5 +1,4 @@
 import argparse
-import copy
 import os
 import random
 import time
@@ -16,15 +15,17 @@ from mmdet3d.datasets import build_dataset
 from mmdet3d.models import build_model
 from mmdet3d.utils import convert_sync_batchnorm, get_root_logger, recursive_eval
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"
-
 
 def main():
     dist.init()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("config", metavar="FILE", help="config file")
-    parser.add_argument("--run-dir", metavar="DIR", help="run directory")
+    parser.add_argument("config", help="config file")
+    parser.add_argument("--run-dir", required=False, help="run directory")
+    parser.add_argument("--auto-run-dir", required=False, help="auto-run directory")
+    parser.add_argument(
+        "--skip-if-exists", required=False, action="store_true", help="skip if exists"
+    )
     args, opts = parser.parse_known_args()
 
     configs.load(args.config, recursive=True)
@@ -35,10 +36,20 @@ def main():
     torch.backends.cudnn.benchmark = cfg.cudnn_benchmark
     torch.cuda.set_device(dist.local_rank())
 
+    if args.auto_run_dir is not None:
+        auto_run_dir = os.path.join(args.auto_run_dir, create_auto_dir_name(cfg))
+        if os.path.exists(auto_run_dir):
+            if args.skip_if_exists:
+                print(f"Skip {auto_run_dir} as it already exists.")
+                return
+            auto_run_dir = auto_run_dir + "_" + configs.hash()[:8] + "_" + str(int(time.time()))
+        args.run_dir = auto_run_dir
+
     if args.run_dir is None:
         args.run_dir = auto_set_run_dir()
     else:
         set_run_dir(args.run_dir)
+
     cfg.run_dir = args.run_dir
 
     # dump config
@@ -80,6 +91,87 @@ def main():
         validate=True,
         timestamp=timestamp,
     )
+
+
+def create_auto_dir_name(cfg) -> str:
+    """
+    Create a directory name for auto-run mode.
+
+    Args:
+        cfg (Config): Config instance.
+
+    Returns:
+        str: Directory name.
+    """
+    out = []
+    models = []
+    info = []
+
+    if cfg["model"]["encoders"]["lidar"] is None:
+        modality = "c"  # means it is camera only
+    elif cfg["model"]["encoders"]["camera"] is None:
+        modality = "l"  # means it is lidar only
+    else:
+        modality = "lc"  # means it is fusion
+
+    # fusion info
+    if modality == "lc":
+        # models
+        fuser = cfg["model"].get("fuser", {}).get("type", None)
+        if fuser is not None:
+            models.append(fuser.lower())
+
+    temporal_fuser = cfg["model"].get("temporal_fuser", {}).get("type", None)
+    if temporal_fuser is not None:
+        models.append(temporal_fuser.lower())
+
+    # lidar info
+    if modality.count("l") > 0:
+        # models
+        if cfg["model"]["encoders"]["lidar"]["backbone"]["type"] == "SparseEncoder":
+            models.append("voxelnet")
+        else:
+            raise NotImplementedError
+
+    # camera info
+    if modality.count("c") > 0:
+        # models
+        if cfg["model"]["encoders"]["camera"]["backbone"]["type"] == "SwinTransformer":
+            models.append("swint")
+        else:
+            raise NotImplementedError
+        if cfg["model"]["encoders"]["camera"]["vtransform"]["type"] == "DepthLSSTransform":
+            models.append("depthlss")
+        elif cfg["model"]["encoders"]["camera"]["vtransform"]["type"] == "LSSTransform":
+            models.append("lss")
+        else:
+            raise NotImplementedError
+        # image size
+        info.append(f"{cfg['image_size'][0]}x{cfg['image_size'][1]}")
+
+    # grid size
+    info.append(f"{cfg['grid_size'][0]}z{cfg['grid_size'][2]}")
+    # voxel size
+    info.append(
+        f"{str(cfg['voxel_size'][0]).replace('.', 'xy')}-{str(cfg['voxel_size'][2]).replace('.', 'z')}"
+    )
+
+    # score threshold
+    info.append(str(cfg["score_threshold"]).replace(".", "st"))
+
+    # gt mode stop epoch
+    if cfg["gt_paste_stop_epoch"] not in [None, -1]:
+        info.append(f"gtp{cfg['gt_paste_stop_epoch']}")
+
+    # grid mask
+    if cfg["augment2d"]["gridmask"]["max_epochs"] not in [None, -1]:
+        info.append(f"gm{cfg['augment2d']['grid_mask']['max_epochs']}")
+
+    out.append(modality)
+    out.extend(models)
+    out.extend(info)
+
+    return "-".join(out)
 
 
 if __name__ == "__main__":
